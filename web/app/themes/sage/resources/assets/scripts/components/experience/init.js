71f8b349ed063d6422afb248aace150db70ca80a
Valeriy <example@gmail.com>
2020-11-13 17:59:07 +0300

    Более точная анимация карты

--- web/app/themes/sage/resources/assets/scripts/components/experience/init.js
+++ web/app/themes/sage/resources/assets/scripts/components/experience/init.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
-    let partnerCardPlace = document.querySelector('.partner__left');
-    setInterval(() => {
-        console.log(partnerCardPlace.getBoundingClientRect().left);
-    }, 33.3);
+
 });
