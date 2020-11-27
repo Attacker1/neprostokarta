43afc0de9c022f12fd6eb49bd2608dcfdd3b6b18
erlementovich <BycsorucAvyith1>
2020-11-27 13:25:11 +0300

    fix images url

--- web/app/themes/sage/resources/views/components/experience/animation.blade.php
+++ web/app/themes/sage/resources/views/components/experience/animation.blade.php
@@ -45,8 +45,19 @@
 
     const textAnim = TweenMax.fromTo(text, 3, {opacity: 0}, {opacity: 1});
     const cardAnim = TweenMax.fromTo(card, 3, {opacity: 0}, {opacity: 1});
-    const helloCardAnim = TweenMax.fromTo(helloCard, 3, {scaleX: 1, scaleY:1, rotation:7}, {scaleX:0.5, scaleY:0.5, rotation:-160, opacity: cardOpacity});
-    const helloCardScale = TweenMax.to(helloCard, 1, {scaleX:10, scaleY:10, rotation:-90, zIndex:2, display:'none'});
+    const helloCardAnim = TweenMax.fromTo(helloCard, 3, {scaleX: 1, scaleY: 1, rotation: 7}, {
+      scaleX: 0.5,
+      scaleY: 0.5,
+      rotation: -160,
+      opacity: cardOpacity
+    });
+    const helloCardScale = TweenMax.to(helloCard, 1, {
+      scaleX: 10,
+      scaleY: 10,
+      rotation: -90,
+      zIndex: 2,
+      display: 'none'
+    });
 
 
     // Анимация видео
@@ -82,22 +93,19 @@
       });
 
       scene.on('progress', e => {
-        if(e.progress > 0.25) {
+        if (e.progress > 0.25) {
           item.eq(1).addClass('experience__item-active').addClass('experience__item-green');
-        }
-        else {
+        } else {
           item.eq(1).removeClass('experience__item-active').removeClass('experience__item-green');
         }
-        if(e.progress > 0.5) {
+        if (e.progress > 0.5) {
           item.eq(2).addClass('experience__item-active').addClass('experience__item-green');
-        }
-        else {
+        } else {
           item.eq(2).removeClass('experience__item-active').removeClass('experience__item-green');
         }
-        if(e.progress > 0.75) {
+        if (e.progress > 0.75) {
           item.eq(3).addClass('experience__item-active').addClass('experience__item-green');
-        }
-        else {
+        } else {
           item.eq(3).removeClass('experience__item-active').removeClass('experience__item-green');
         }
       });
@@ -107,9 +115,12 @@
       let item = $('.experience__item');
       const context = video.getContext('2d');
 
-      const currentFrame = index => (
-        `${window.wp_data.experience_img}card-animation_${index.toString().padStart(3, '0')}.jpg`
-      )
+      const currentFrame = index => {
+        const indexStr = index.toString().padStart(3, '0');
+        return (
+          window.wp_data.imagesUrls[indexStr]
+        )
+      };
 
       const frameCount = 72;
 
@@ -117,7 +128,7 @@
       video.width = 600;
       const img = new Image();
       img.src = currentFrame(1);
-      img.onload = function(){
+      img.onload = function () {
         context.drawImage(img, 0, 0)
       }
 
@@ -127,12 +138,11 @@
       }
 
       scene.on('progress', function (e) {
-        requestAnimationFrame( () => updateImage(Math.round(e.progress * frameCount)))
+        requestAnimationFrame(() => updateImage(Math.round(e.progress * frameCount)))
       })
 
 
-
-      scene.on('end', function() {
+      scene.on('end', function () {
         $('.experience__card-img').toggleClass('experience__card-active');
       });
 
@@ -155,8 +165,8 @@
       triggerElement: intro,
       triggerHook: 0,
     })
-    .setTween(timeline)
-    .addTo(controller);
+      .setTween(timeline)
+      .addTo(controller);
 
     let scene3 = new ScrollMagic.Scene({
       duration: 2000,
@@ -174,8 +184,6 @@
     })
       .setTween(helloCardScale)
       .addTo(controller);
-
-
 
 
     // partner section
